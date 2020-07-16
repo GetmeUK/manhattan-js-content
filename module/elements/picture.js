b2d1c183c7aff8b78bdd94d996b23a051456ba28
Anthony <ant@getme.co.uk>
2020-07-16 22:51:50 +0100

    Added picture element.

--- module/elements/picture.js
+++ module/elements/picture.js
@@ -4,11 +4,19 @@
  */
 export class PictureElement extends ContentEdit.Element {
 
-    constructor(attributes, src) {
+    constructor(attributes, src, sources) {
         super('picture', attributes)
 
         // The image source
         this._src = src
+
+        // The source for the picture
+        this._sources = []
+        if (sources) {
+            for (const source of sources) {
+                this._sources.push(Object.assign(source))
+            }
+        }
     }
 
     cssTypeName() {
@@ -24,19 +32,33 @@ export class PictureElement extends ContentEdit.Element {
     }
 
     html(indent='') {
+        const le = ContentEdit.LINE_ENDINGS
+
         const alt = `alt="${this._attributes['alt'] || ''}"`
         const attributes = this._attributesToString()
         const img = `${indent}<img src="${this.src()}" ${alt}>`
-        const le = ContentEdit.LINE_ENDINGS
 
-        return [
-            `${indent}<${this.tagName()} ${attributes}>${le}`,
-            `${ContentEdit.INDENT}${img}${le}`,
-            `${indent}</${this.tagName()}>`
-        ].join('')
+        const lines = []
+        lines.push(`${indent}<${this.tagName()} ${attributes}>${le}`)
+        for (const source of this._sources) {
+            let mediaAttr = ''
+            if (source.media) {
+                mediaAttr = ` media="${source.media}"`
+            }
+
+            lines.push(`${indent}${ContentEdit.INDENT}`
+                + `<source srcset="${source.srcset}"`
+                + `{mediaAttr}>${le}`)
+        }
+        lines.push(`${ContentEdit.INDENT}${img}${le}`)
+        lines.push(`${indent}</${this.tagName()}>`)
+
+        lines.join('')
     }
 
     mount() {
+
+        // Picture
         this._domElement = document.createElement(this.tagName())
 
         for (let [name, value] of Object.entries(this._attributes)) {
@@ -46,11 +68,47 @@ export class PictureElement extends ContentEdit.Element {
         }
         this._domElement.setAttribute('class', this._attributes['class'])
 
+        // Sources
+        for (const source of this._sources) {
+            const sourceElm = document.createElement('source')
+            sourceElm.setAttr('srcset', source.srcset)
+            if (source.media) {
+                sourceElm.setAttr('media', source.media)
+            }
+        }
+
+        // Image
         const imgElm = document.createElement('img')
         imgElm.src = this.src()
         this._domElement.appendChild(imgElm)
 
         super.mount()
+    }
+
+    sources(sources) {
+        let source = null
+
+        if (typeof sources === 'undefined') {
+            const cloned = []
+            for (source of this._sources) {
+                cloned.push(Object.assign(source))
+            }
+            return cloned
+        }
+
+        this._sources = []
+        for (source of this._sources) {
+            this._sources.push(Object.assign(source))
+        }
+
+        if (this.isMounted()) {
+            this.unmount()
+            this.mount()
+        }
+
+        this.taint()
+
+        return null
     }
 
     src(src) {
@@ -86,20 +144,27 @@ export class PictureElement extends ContentEdit.Element {
 
         let src = ''
         let alt = ''
+        const sources = []
         for (const childNode of [...domElement.childNodes]) {
-            if (childNode.nodeType === 1
-                && childNode.tagName.toLowerCase() === 'img') {
+            if (childNode.nodeType !== 1) {
+                if (childNode.tagName.toLowerCase() === 'img') {
+                    src = childNode.getAttribute('src') || ''
+                    alt = childNode.getAttribute('alt') || ''
 
-                src = childNode.getAttribute('src') || ''
-                alt = childNode.getAttribute('alt') || ''
-                break
+                } else if (childNode.tagName.toLowerCase() === 'source') {
+                    let source = {'srcset': childNode.getAttribute('srcset')}
+                    if (childNode.getAttribute('media')) {
+                        source['media'] = childNode.getAttribute('media')
+                    }
+                    sources.push(source)
+                }
             }
         }
 
         attributes = PictureElement.getDOMElementAttributes(domElement)
         attributes['alt'] = alt
 
-        return new PictureElement(attributes, src)
+        return new PictureElement(attributes, src, sources)
     }
 }
 
