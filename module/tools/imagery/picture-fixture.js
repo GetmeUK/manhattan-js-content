8330fd339749f872992dffdb3cb0ed58bfca76e3
Anthony <ant@getme.co.uk>
2020-07-22 00:25:53 +0100

    Started to plan out the next steps for image set support

--- module/tools/imagery/picture-fixture.js
+++ module/tools/imagery/picture-fixture.js
@@ -1,8 +1,46 @@
 import * as contenttools from 'ContentTools'
 import * as $ from 'manhattan-essentials'
 
+import {ImageSetEditor} from './../../ui/image-set-editor'
 import {ImageUploader} from './../../ui/image-uploader'
 import {convertTransforms} from './utils'
+
+
+// Private
+
+/**
+ * Create a UI to allow a user to edit an image.
+ */
+function editImage(
+    elm,
+    onDone,
+    uploadURL,
+    assetKeys,
+    imageURLs,
+    localTransforms,
+    transition
+) {
+    let proxy = $.closest(elm.domElement(), '[data-mh-image-set-proxy]')
+    const versions = JSON.parse(proxy.dataset.mhImageSetVersions)
+    const versionLabels = JSON.parse(proxy.dataset.mhImageSetVersionLabels)
+    const cropAspectRatios
+        = JSON.parse(proxy.dataset.mhImageSetCropAspectRatios)
+
+    // Create UI to allow the user to edit the image
+    const imageEditor = new ImageSetEditor(
+        assetKeys,
+        imageURLs,
+        localTransforms,
+        versions,
+        versionLabels,
+        cropAspectRatios,
+        uploadURL,
+        $.one('[data-mh-content-ui]')
+    )
+    imageEditor.init()
+    imageEditor.show(transition)
+
+}
 
 
 // Public
@@ -36,8 +74,33 @@ export function apply(elm, onDone, uploadURL) {
                     // Destroy the image uploader
                     imageUploader.destroy()
 
-                    // @@ Edit the image uploaded along with providing options
-                    // to switch version.
+                    let proxy = $.closest(
+                        elm.domElement(),
+                        '[data-mh-image-set-proxy]'
+                    )
+                    const versions
+                        = JSON.parse(proxy.dataset.mhImageSetVersions)
+
+                    // Switch to the editing environment
+                    const {asset} = event
+                    const meta = event.asset['core_meta']['image']
+                    const {variations} = event.asset
+
+                    const assetKeys = {}
+                    assetKeys[versions[0]] = asset['key']
+
+                    const imageURLs = {}
+                    imageURLs[versions[0]] = variations['--draft--']['url']
+
+                    editImage(
+                        elm,
+                        onDone,
+                        uploadURL,
+                        assetKeys,
+                        imageURLs,
+                        {},
+                        false
+                    )
                 },
                 'cancel': (event) => {
                     // User cancelled uploading the image
@@ -62,3 +125,45 @@ export function apply(elm, onDone, uploadURL) {
         )
     }
 }
+
+
+// # TODO
+//
+// - Support for converting transforms between those the editor understands
+//   and those manhattan understands. As we need to be able to reproduce the
+//   local transforms within the editor we need to be able to convert between
+//   not just one way.
+//
+// - Build a custom VersionEditor as a base, we'll have a bunch of work to do
+//   to this but initially don't create the controls and get the custom CSS in
+//   place. Switch to using this in the image set editor.
+//
+// - Send the image set editor the local transforms (in editor format).
+//
+// - Get the image set editor to open up the version editor for the base
+//   version initially. Don't use a version property for this it doesn't have
+//   enough control as we can't send it the the transition flag. Instead create
+//   an edit method that accepts a version and transition flag, call this
+//   instead of show (which can be removed as a method).
+//
+// - Create a custom version control in our version editor. When the version
+//   is changed this should trigger an event against overlay that the image set
+//   editor can listen to and on receiving can use the edit method to swich to
+//   editing a different version, we'll need to send the version labels dict
+//   and the current version to the version editor.
+//
+// - Sources will hold the information relevant for each version of the image
+//   set, e.g:
+//
+//   <picture>
+//       <source
+//           srcset="..."
+//           media="(...)"
+//           data-mh-version="l"
+//           data-mh-asset-key="..." // Optional if uses base version
+//           data-mh-draft="..." // Optional if uses base version
+//           data-base-transforms="..." // Optional (should always have a value)
+//           data-local-transforms="..." // Contains the crop/rotate transforms
+//       >
+//   </picture>
+//
--- module/ui/image-set-editor.js
+++ module/ui/image-set-editor.js
@@ -1,3 +1,7 @@
+import * as $ from 'manhattan-essentials'
+
+import {ImageEditor} from 'manhattan-assets/module/ui/image-editor'
+
 
 /**
  * A manager that allows users to edit versions of an image set (including
@@ -5,20 +9,122 @@
  */
 export class ImageSetEditor {
 
-    constructor(uploadURL, container=null) {
-        super(container)
+    constructor(
+        assetKeys,
+        imageURLs,
+        versions,
+        versionLabels,
+        cropAspectRatios,
+        uploadURL,
+        container=null
+    ) {
+
+        // @@ Local transforms???
+
+        // A list of versions defined for the image set
+        this._versions = versions.slice()
+
+        // A map of labels for each version in the image set
+        this._versionLabels = Object.assign({}, versionLabels)
+
+        // A map of crop ratios to be applied for each version in the image set
+        this._cropAspectRatios = Object.assign({}, cropAspectRatios || {})
 
         // The URL images should be uploaded to
         this._uploadURL = uploadURL
+
+        // A map of asset keys for versions
+        this._assetKeys = Object.assign({}, assetKeys)
+
+        // A map of image URLs for versions
+        this._imageURLs = Object.assign({}, imageURLs)
+
+        // The current image editor (the image set editor displays)
+        this._editor = null
+
+        // The current version of the image set being edited
+        this._version = this.baseVersion
+
+        // Domain for related DOM elements
+        this._dom = {container}
     }
 
-    // - Versions
-    // - Crop ratios
-    // - Preview size
-    // - Container
+    // Getters & Setters
 
-    // imageURL,
-    // cropRatio,
-    // [600, 600],
-    // $.one('[data-mh-content-ui]')
+    get baseVersion() {
+        return this._versions[0]
+    }
+
+    get version() {
+        return this._version
+    }
+
+    set version(version) {
+        if (version !== this._version) {
+            return
+        }
+
+        // Change the version
+        this._version = version
+
+        // ?? What if version is called before we've initialized the editor?
+
+        // Create a new editor for this version of the image set
+        this._imageEditor = new ImageEditor(
+            this.getImageURL(version),
+            this._cropAspectRatios[version],
+            true,
+            [600, 600],
+            this._dom.container
+        )
+        this._imageEditor.init()
+    }
+
+    // Public methods
+
+    /**
+     * Remove the image set editor.
+     */
+    destroy() {
+        console.log(this, 'destroy')
+    }
+
+    /**
+     * Get an image URL for a version of the image set.
+     */
+    getImageURL(version) {
+        if (version in this._imageURLs) {
+            return this._imageURLs[version]
+        }
+        return this._imageURLs[this.baseVersion]
+    }
+
+    /**
+     * Hide the image set editor.
+     */
+    hide() {
+        console.log(this, 'hide')
+    }
+
+    /**
+     * Initialize the image set editor.
+     */
+    init() {
+        this.version = this.baseVersion
+    }
+
+    /**
+     * Show the image set editor.
+     */
+    show(transition) {
+        this._imageEditor.show()
+
+        // If switching from the uploader to the editor we don't
+        // transition (using a fade).
+        if (!transition) {
+            this._imageEditor.overlay.classList.add('mh-overlay--no-fade')
+            $.dispatch(this._imageEditor.overlay, 'transitionend')
+            this._imageEditor.overlay.classList.remove('mh-overlay--no-fade')
+        }
+    }
 }
